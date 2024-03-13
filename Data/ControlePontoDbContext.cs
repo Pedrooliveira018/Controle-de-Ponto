a15d46be5e26a920cf36de624fbd92fbd358c0a1
Aluno <Aluno@DESKTOP-J3E62P5>
2024-03-12 21:57:02 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ControlePontoDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Controle_de_Ponto.Modelos;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Controle_de_Ponto.Data
+{
+    internal class ControlePontoDbContext : DbContext
+    {
+        public ControlePontoDbContext(DbContextOptions<ControlePontoDbContext> opitions) : base(opitions)
+        {
+        }  
+
+        public DbSet<Cargo> Cargos { get; set; }
+
+    }
+}
---
+++ Modelos/Cargo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle_de_Ponto.Modelos
+{
+    internal class Cargo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+        public string Descricao { get; set; }
+        public decimal CargaHoraria { get; set; }   
+    }
+}
